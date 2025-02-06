74cf450ff295aac1936675785375037a5f9acc1f
Matthew Jeshua Martin <matthew-martin@programmer.net>
2025-02-06 13:45:46 -0500

    adding default.vert to use.

--- phantasm-core-engine/src/main/java/com/horrorcoresoftware/Main.java
+++ phantasm-core-engine/src/main/java/com/horrorcoresoftware/Main.java
@@ -1,7 +1,19 @@
 package com.horrorcoresoftware;
 
+import com.horrorcoresoftware.core.Engine;
+import com.horrorcoresoftware.core.debug.Logger;
+import com.horrorcoresoftware.exceptions.EngineInitException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = Logger.getInstance();
+        Engine engine = new Engine();
+        try {
+            engine.initialize();
+            engine.start();
+        } catch (EngineInitException e) {
+            logger.logError("Main", "Failed to initialize the engine", e);
+            System.exit(1);
+        }
     }
 }
